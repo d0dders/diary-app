03fa640a9be7de7cb4d6aed8eb2bb46120ed9b2c
d0dders <dodders@outlook.com>
2020-11-07 21:18:16 +0000

    Added some spacing and cls between screens

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 import datetime
+import os
 import sys
 
 from peewee import *
@@ -27,12 +28,14 @@ def menu_loop():
     choice = None
 
     while choice != 'q':
+        clear()
         print("Enter 'q' to quit.")
         for key, value in menu.items():
             print('{}) {}'.format(key, value.__doc__))
         choice = input('Action: ').lower().strip()
 
         if choice in menu:
+            clear()
             menu[choice]()
 
 
@@ -55,9 +58,11 @@ def view_entries(search_query=None):
 
     for entry in entries:
         timestamp = entry.timestamp.strftime('%A %B %d, %Y %I:%M%p')
+        clear()
         print(timestamp)
         print('='*len(timestamp))
         print(entry.content)
+        print('\n\n' + '='*len(timestamp))
         print('n) next entry')
         print('d) delete entry')
         print('q) return to main menu')
@@ -80,6 +85,9 @@ def delete_entry(entry):
         entry.delete_instance()
         print("Entry deleted!")
 
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 menu = OrderedDict([
